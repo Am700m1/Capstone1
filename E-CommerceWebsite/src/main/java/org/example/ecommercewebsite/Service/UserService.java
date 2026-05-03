60ad923e83d050d8f1f13a48baf772ba82af094a
Ammar <s202024140@kfupm.edu.sa>
2026-05-03 16:01:14 +0300

    Added UserService Class with CRUD

---
+++ E-CommerceWebsite/src/main/java/org/example/ecommercewebsite/Service/UserService.java
@@ -0,0 +1,43 @@
+package org.example.ecommercewebsite.Service;
+
+import org.example.ecommercewebsite.Model.User;
+
+import java.util.ArrayList;
+
+public class UserService {
+    ArrayList<User> users = new ArrayList<>();
+
+    public Boolean addUser(User user){
+        for(User user1: users){
+            if(user1.getId().equalsIgnoreCase(user.getId())){
+                return false;
+            }
+        }
+        users.add(user);
+        return true;
+    }
+
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+
+    public Boolean updateUser(String id, User user){
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getId().equalsIgnoreCase(id)){
+                users.set(i, user);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean deleteUser(String id){
+        for(User user: users){
+            if(user.getId().equalsIgnoreCase(id)){
+                users.remove(user);
+                return true;
+            }
+        }
+        return false;
+    }
+}
