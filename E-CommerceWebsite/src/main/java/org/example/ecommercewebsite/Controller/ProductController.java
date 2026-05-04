6b0f7e9f3d8013c262bbf0988286c9f4ae122ed3
Ammar <s202024140@kfupm.edu.sa>
2026-05-04 10:34:38 +0300

    Added Product controller and modified Product Service to check for the existance of category id before adding or updating a product

---
+++ E-CommerceWebsite/src/main/java/org/example/ecommercewebsite/Controller/ProductController.java
@@ -0,0 +1,64 @@
+package org.example.ecommercewebsite.Controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.ecommercewebsite.Api.ApiResponse;
+import org.example.ecommercewebsite.Model.Product;
+import org.example.ecommercewebsite.Service.CategoryService;
+import org.example.ecommercewebsite.Service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/get-products")
+    public ResponseEntity<?> getProducts(){
+        ArrayList<Product> products = productService.getProducts();
+
+        if(products.isEmpty()){
+            return ResponseEntity.status(400).body(new ApiResponse("There is no products added yet!"));
+        }else{
+            return ResponseEntity.status(200).body(products);
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addProduct(@RequestBody @Valid Product product, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(400).body(errors.getFieldError().getDefaultMessage());
+        }
+
+        return switch (productService.addProduct(product)) {
+            case 1 -> ResponseEntity.status(200).body(new ApiResponse("Product was added successfully"));
+            case 2 -> ResponseEntity.status(400).body(new ApiResponse("Another product with the same id exists!"));
+            default -> ResponseEntity.status(400).body(new ApiResponse("Category ID does not exist!"));
+        };
+    }
+
+    public ResponseEntity<?> updateProduct(@PathVariable String id, @RequestBody @Valid Product product, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.status(400).body(errors.getFieldError().getDefaultMessage());
+        }
+
+        return switch (productService.updateProduct(id, product)){
+            case 1 -> ResponseEntity.status(200).body(new ApiResponse("Product was updated successfully"));
+            case 2 -> ResponseEntity.status(400).body(new ApiResponse("Category ID does not exist!"));
+            default -> ResponseEntity.status(400).body(new ApiResponse("Product was not found!"));
+        };
+    }
+
+    public ResponseEntity<?> deleteProduct(@PathVariable String id){
+        if(productService.deleteProduct(id)){
+            return ResponseEntity.status(200).body(new ApiResponse("Product was deleted successfully"));
+        }else{
+            return ResponseEntity.status(400).body(new ApiResponse("Product was not found!"));
+        }
+    }
+}
--- E-CommerceWebsite/src/main/java/org/example/ecommercewebsite/Service/ProductService.java
+++ E-CommerceWebsite/src/main/java/org/example/ecommercewebsite/Service/ProductService.java
@@ -1,34 +1,56 @@
 package org.example.ecommercewebsite.Service;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.ecommercewebsite.Model.Category;
 import org.example.ecommercewebsite.Model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
+@RequiredArgsConstructor
+
 public class ProductService {
+    private final CategoryService categoryService;
+    ArrayList<Category> categories = categoryService.getCategories();
+
+    @Getter
     ArrayList<Product> products = new ArrayList<>();
 
-    public Boolean addProduct(Product product){
+    public Integer addProduct(Product product){
         for(Product product1: products){
             if(product1.getId().equalsIgnoreCase(product.getId())){
-                return false;
+                return 2;
             }
         }
-        products.add(product);
-        return true;
-    }
-
-    public ArrayList<Product> getProducts(){
-        return products;
-    }
-
-    public Boolean updateProduct(String id, Product product){
-        for(int i = 0; i< products.size(); i++){
-            if(products.get(i).getId().equalsIgnoreCase(id)){
-                products.set(i, product);
-                return true;
+        for(Category category: categories){
+            if(product.getCategoryID().equalsIgnoreCase(category.getId())){
+                products.add(product);
+                return 1;
             }
         }
-        return false;
+        return 0;
+    }
+
+    public Integer updateProduct(String id, Product product){
+        Boolean exist = false;
+        for(Category category: categories){
+            if(category.getId().equalsIgnoreCase(product.getId())){
+                exist = true;
+            }
+        }
+        if(exist) {
+            for (int i = 0; i < products.size(); i++) {
+                if (products.get(i).getId().equalsIgnoreCase(id)) {
+                    products.set(i, product);
+                    return 1;
+                }
+            }
+        }else{
+            return 2;
+        }
+        return 0;
     }
 
     public Boolean deleteProduct(String id){
