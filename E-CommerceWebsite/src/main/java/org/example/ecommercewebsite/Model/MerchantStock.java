3ad6efbccaa7265faec78dd326df5cd52afe2a6c
Ammar <s202024140@kfupm.edu.sa>
2026-05-04 14:00:03 +0300

    fixed stereotype error NotEmpty() -> NotNull()

--- E-CommerceWebsite/src/main/java/org/example/ecommercewebsite/Model/MerchantStock.java
+++ E-CommerceWebsite/src/main/java/org/example/ecommercewebsite/Model/MerchantStock.java
@@ -1,9 +1,6 @@
 package org.example.ecommercewebsite.Model;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -20,7 +17,7 @@ public class MerchantStock {
     @NotEmpty(message = "Merchant ID must be filled!")
     private String merchantId;
 
-    @NotEmpty(message = "Stock must be filled!")
+    @NotNull(message = "Stock must be filled!")
     @Positive(message = "Stock must be a positive number!")
     @Min(value = 11, message = "Stock must be more than 10 initially!")
     private Integer stock;
